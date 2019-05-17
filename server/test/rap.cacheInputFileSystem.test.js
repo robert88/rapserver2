1e30bcc66c49279896496aae21861ed95faf3dbe
robert88 <841354162@qq.com>
2019-05-17 18:21:34 +0800

    Add files via upload

--- server/test/rap.cacheInputFileSystem.test.js
+++ server/test/rap.cacheInputFileSystem.test.js
@@ -6,85 +6,69 @@ const pt = require("path")
 
 const CachedInputFileSystem = require("../lib/node_modules/enhanced-resolve/lib/CachedInputFileSystem");
 
-
-//测试exist
-test('rap cacheInputFileSystem exists', (done) => {
-	let asyncExist1,asyncExist2;
-	let asyncExistTrue,asyncExistFalse;
-	rap.cacheInputFileSystem.exists(pt.resolve(__dirname,"../test"),function(data){
-		asyncExist1 = true;
-		asyncExistTrue =data;
-		doneComplete();
-	})
-	rap.cacheInputFileSystem.exists("../test",function(data){
-		asyncExist2 = true;
-		asyncExistFalse =data;
-		doneComplete();
-	})
-	function doneComplete(){
-	if(asyncExist1&&asyncExist2){
-			expect(isExistTestTrue).toBe(true);
-			expect(isExistTestFalse).toBe(false);
-			expect(asyncExistTrue).toBe(true);
-			expect(asyncExistFalse).toBe(false);
-			done();
-		}
-	}
-	let isExistTestTrue = rap.cacheInputFileSystem.existsSync(pt.resolve(__dirname,"../test"));
-	let isExistTestFalse = rap.cacheInputFileSystem.existsSync("../test");
-});
+var apis = ["readdir","stat","readFile","readlink","exists","readJson","readData","findFile","findDir","getSize","getModify","isDir","isFile"];
 
 //必须提供以下api
 test('rap cacheInputFileSystem api', () => {
-	["readdir","stat","readFile","readlink","exists","readJson","readData","findFile","findDir","getSize","getModify","isDir","isFile"].forEach(type=>{
-		console.log(type,typeof rap.cacheInputFileSystem[type])
+	apis.forEach(type=>{
 		expect(typeof rap.cacheInputFileSystem[type]).toBe("function");
 		expect(typeof rap.cacheInputFileSystem[type+"Sync"]).toBe("function");
 	})
 });
 
-//必须提供以下api
-test('rap cacheInputFileSystem storage cache sync', () => {
-	var inputFileSystem = {};
-	["readdir","stat","readFile","readlink","exists"].forEach(type=>{
-		inputFileSystem[type+"Sync"] = jest.fn();
-	});
-	var cacheInput = new CachedInputFileSystem(inputFileSystem);
-	var testPath = pt.resolve(__dirname,"../test");
-	["readdir","stat","readFile","readlink","exists"].forEach(type=>{
-		[1,2,3,4].forEach(()=>{
-			cacheInput[type+"Sync"](testPath);
-		});
-		expect(inputFileSystem[type+"Sync"]).toHaveBeenCalledTimes(1);
-	});
-})
+//测试readdir操作，读取当前文件夹全部信息
+test(`rap cacheInputFileSystem api readdir`, (done) => {
+	var dir = pt.resolve(__dirname,"./readDir");
 
-test('rap cacheInputFileSystem extend api', () => {
-	let inputFileSystem ={};
-	["readdir","stat","readFile","readlink","exists"].forEach(type=>{
-		inputFileSystem[type+"Sync"] = jest.fn();
-	});
-	var cacheInput = new CachedInputFileSystem(inputFileSystem);
-	//扩展的接口
-	var testPath = pt.resolve(__dirname,"../test");
-	["readJson","readData","getSize","getModify","isDir","isFile"].forEach(type=>{
-		[1,2,3,4].forEach(()=>{
-			cacheInput[type+"Sync"](testPath);
-		});
-		expect(inputFileSystem["readFileSync"]).toHaveBeenCalledTimes(1);
-	});
-	["findFile","findDir"].forEach(type=>{
-		[1,2,3,4].forEach(()=>{
-			cacheInput[type+"Sync"](testPath);
-		});
-		expect(inputFileSystem["readdirSync"]).toHaveBeenCalledTimes(1);
-	});
+	rap.cacheInputFileSystem.readdir(dir,function(err,data){
+		//toBe不能比较对象
+		// expect(data).toBe(["deep1", "file2.txt", "file3.js"]);
+		expect(data).toEqual(["deep1", "file2.txt", "file3.js"]);
+		done();
+	})
+	var dataSync = rap.cacheInputFileSystem.readdirSync(dir);
+	expect(dataSync).toEqual(["deep1", "file2.txt", "file3.js"]);
 
-	["getSize","getModify","isDir","isFile"].forEach(type=>{
-		[1,2,3,4].forEach(()=>{
-			cacheInput[type+"Sync"](testPath);
-		});
-		expect(inputFileSystem["statSync"]).toHaveBeenCalledTimes(1);
-	});
+});
 
-})
+
+
+//测试readdir操作，读取当前文件夹全部信息
+test(`rap cacheInputFileSystem api findDir`, (done) => {
+	var dir = pt.resolve(__dirname,"./readDir");
+
+	rap.cacheInputFileSystem.findDirSync(dir,function(err,data){
+		expect(data).toEqual(["deep1"]);
+		done();
+	});
+});
+
+
+// //测试exists操作,测试文件是否存在
+// test(`rap cacheInputFileSystem api exists`, (done) => {
+// 	let asyncExist1,asyncExist2;
+// 	let asyncExistTrue,asyncExistFalse;
+// 	rap.cacheInputFileSystem.exists(pt.resolve(__dirname,"../test"),function(data){
+// 		asyncExist1 = true;
+// 		asyncExistTrue =data;
+// 		doneComplete();
+// 	})
+// 	rap.cacheInputFileSystem.exists("../test",function(data){
+// 		asyncExist2 = true;
+// 		asyncExistFalse =data;
+// 		doneComplete();
+// 	})
+// 	function doneComplete(){
+// 	if(asyncExist1&&asyncExist2){
+// 			expect(isExistTestTrue).toBe(true);
+// 			expect(isExistTestFalse).toBe(false);
+// 			expect(asyncExistTrue).toBe(true);
+// 			expect(asyncExistFalse).toBe(false);
+// 			done();
+// 		}
+// 	}
+// 	let isExistTestTrue = rap.cacheInputFileSystem.existsSync(pt.resolve(__dirname,"../test"));
+// 	let isExistTestFalse = rap.cacheInputFileSystem.existsSync("../test");
+// });
+
+
