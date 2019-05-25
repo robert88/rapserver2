86a9772e3a32ef0ce8874f11b72cb846d57f8d04
robert88 <841354162@qq.com>
2019-05-25 16:35:37 +0800

    Delete rap.inputFileSystem.js

--- server/lib/rap.inputFileSystem.js
+++
@@ -1,9 +0,0 @@
-
-global.rap = global.rap||{};
-
-//定义5s以内的缓存
-
-const NodeJsInputFileSystem = require("enhanced-resolve/lib/NodeJsInputFileSystem");
-
-rap.inputFileSystem = new NodeJsInputFileSystem();
-
