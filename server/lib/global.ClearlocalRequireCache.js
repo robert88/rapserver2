686c7bb13e72a1270eb7466c6203ce18d912a895
robert88 <841354162@qq.com>
2019-05-28 17:54:56 +0800

    Delete global.ClearlocalRequireCache.js

--- server/lib/global.ClearlocalRequireCache.js
+++
@@ -1,9 +0,0 @@
-
-global.clearLocalRequireCache = function(file){
-    var requireFile = localRequire(file,true);
-    try{
-        delete require.cache[require.resolve(requireFile)];
-    }catch (e) {
-      console.log(e.stack)
-    }
-}
