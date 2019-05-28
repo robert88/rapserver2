6863d0805cff1b18bb34ee8edf88000aefe0d4eb
robert88 <841354162@qq.com>
2019-05-28 17:55:48 +0800

    Delete rap.toPath.js

--- server/lib/rap.toPath.js
+++
@@ -1,7 +0,0 @@
-    //统一路径格式
-    rap.toPath = (path)=>{
-        if(!path|| /^(http:|https:|\/\/|\\\\)/.test(path)){
-            return path;
-        }
-        return path.replace(/(\/|\\)+/g,"/").replace(/(\/|\\)$/,"");
-    }
