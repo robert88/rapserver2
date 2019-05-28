aa5af6bb2dc74360de8446b10970f037bbc56087
robert88 <841354162@qq.com>
2019-05-28 17:56:57 +0800

    Delete rap.cacheInputFileSystem.js

--- server/lib/rap.cacheInputFileSystem.js
+++
@@ -1,8 +0,0 @@
-
-global.rap = global.rap||{};
-
-const CachedInputFileSystem = require("enhanced-resolve/lib/CachedInputFileSystem");
-
-//定义5s以内的缓存
-
-rap.cacheInputFileSystem = new CachedInputFileSystem(5000);
