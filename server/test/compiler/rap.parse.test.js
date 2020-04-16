d5cfe45e90e26bf8e910af86045a0a96c03e12e9
robert88 <841354162@qq.com>
2020-04-16 18:15:15 +0800

    Add files via upload

--- server/test/compiler/rap.parse.test.js
+++ server/test/compiler/rap.parse.test.js
@@ -4,28 +4,17 @@ localRequire("@/server/lib/global/global.js");
 
 localRequire("@/server/lib/rap/rap.js");
 
-localRequire("@/server/build/rapserver/compiler/rap.parse.js");
+localRequire("@/server/build/compiler/rap.parse.js");
 
 var pt = require("path");
-
-var root = __dirname.toURI().replace("/server/test/compiler","")
-
-var config={
-    suffix:"cn",
-    resolve:function(templatePath,resolvePath){
-        if(resolvePath.trim().toURI().indexOf("/")==0){
-            return root+resolvePath.trim().toURI();
-        }else{
-            return pt.resolve(pt.dirname(templatePath),resolvePath);
-        }
-    }
-}
 
 var parentData={__templateFile__:"test2.html"};
 
 var parentRelativeWatch = {};
 
 var unique = {};
+
+var config={}
 
 var orgHTML = rap.parse.byHtmlFile(pt.resolve(__dirname,"./useTag.html"), config, parentData, parentRelativeWatch, unique)
 console.log(orgHTML)
