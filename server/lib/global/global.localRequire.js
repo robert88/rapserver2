26b1a9531b3c82b12af95dcc99ba6f9684c447aa
robert88 <841354162@qq.com>
2020-11-28 18:04:50 +0800

    Add files via upload

--- server/lib/global/global.localRequire.js
+++ server/lib/global/global.localRequire.js
@@ -1,9 +1,7 @@
-
-
 const getPaths = require("../node_modules/enhanced-resolve/lib/getPaths.js");
 const toPath = require("../node_modules/enhanced-resolve/lib/toPath.js");
 
-const {paths} = getPaths(__dirname);
+const { paths } = getPaths(__dirname);
 
 const pt = require("path");
 const fs = require("fs");
@@ -12,28 +10,27 @@ const fs = require("fs");
 
 
 //将package.json表示跟目录，如果没有使用当前执行环境路径
-let rootPath =  pt.resolve(__dirname) //process.cwd();
+let rootPath = pt.resolve(__dirname) //process.cwd();
 
-paths.forEach(dir=>{
-    var packagePath = pt.resolve(dir,"package.json");
-    if(fs.existsSync(packagePath)){
-        rootPath = dir;
-        return 
-    }
+paths.forEach(dir => {
+  var packagePath = pt.resolve(dir, "package.json");
+  if (fs.existsSync(packagePath)) {
+    rootPath = dir;
+    return
+  }
 })
 
-console.log("@目录："+rootPath)
+console.log("@对应的目录：" + rootPath)
 
 //unload不需要加载模块，只需要转换后的url
-global.localRequire = function(url,unLoad){
-    if(/^@\//.test(url)){
-        url = toPath(url.replace(/^@\//,rootPath+"/"));
-    }
-    //得到路径
-    if(unLoad){
-        return url;
-    }else{
-        return require(url);
-    }
+global.localRequire = function(url, unLoad) {
+  if (/^@\//.test(url)) {
+    url = toPath(url.replace(/^@\//, rootPath + "/"));
+  }
+  //得到路径
+  if (unLoad) {
+    return url;
+  } else {
+    return require(url);
+  }
 }
-
--- server/lib/rap/rap.debounce.js
+++ server/lib/rap/rap.debounce.js
@@ -1,54 +1,53 @@
-global.rap = global.rap||{};
+global.rap = global.rap || {};
 
 /**
  *
  *节流，减少触发次数
  * a_b_c_d_e_f_g
- * 1____1____1
- *触发a、c、f、g
+ * 1____1____1____1____1
+ *触发a、c、d、f、g
  * callback需要执行参数解析
  * */
 var debounce = {};
-rap.debounce = function (callback, time, uuid, params) {
+rap.debounce = function(callback, time, uuid, params) {
 
-	if(!uuid){
-		console.log("请提供唯一的标识");
-	}
+  if (!uuid) {
+    console.log("请提供唯一的标识");
+  }
 
-	time = time || 10000;
+  time = time || 10000;
 
-    let control = debounce[uuid]||[];
+  let control = debounce[uuid] || [];
 
 
-	//锁定10s
-	if (control.length==0) {
+  //锁定10s
+  if (control.length == 0) {
 
-	
-		callback([params],uuid);
 
-		//增加了一个长度
-		control.push(null);
-		
-		//control赋值给debounce之后push是同步操作
-		debounce[uuid] = control;
+    callback([params], uuid);
 
-		setTimeout(function () {
-			
-			if(debounce[uuid].length>1){
-				callback(debounce[uuid].slice(1),uuid);
-			}
-			//清除内存
-			debounce[uuid] = null;
-			delete debounce[uuid];
+    //增加了一个长度
+    control.push(null);
 
-		}, time);
+    //control赋值给debounce之后push是同步操作
+    debounce[uuid] = control;
 
-	}else{
-		//累计参数
-		control.push(params);
-	}
-	//清除内存
-	control=null;
+    setTimeout(function() {
+
+      if (debounce[uuid].length > 1) {
+        callback(debounce[uuid].slice(1), uuid);
+      }
+      //清除内存
+      debounce[uuid] = null;
+      delete debounce[uuid];
+
+    }, time);
+
+  } else {
+    //累计参数
+    control.push(params);
+  }
+  //清除内存
+  control = null;
 
 };
-
