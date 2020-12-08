a5064915c4d39e0d4876f2bbbf9609ef52891eb6
robert88 <841354162@qq.com>
2020-12-08 18:07:27 +0800

    Add files via upload

--- server/lib/global/global.js
+++ server/lib/global/global.js
@@ -1,6 +1,5 @@
-
 localRequire("./extention.Date");
 localRequire("./extention.Number");
 localRequire("./extention.String");
 localRequire("./extention.String.Cmd.Color");
-
+localRequire("./global.ClearlocalRequireCache.js");
