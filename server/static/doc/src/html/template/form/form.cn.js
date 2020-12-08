6b0e5576adfa5aa5483ea8bf36106a20e271fdfa
robert88 <841354162@qq.com>
2020-12-08 18:05:15 +0800

    Add files via upload

--- server/static/doc/src/html/template/form/form.cn.js
+++ server/static/doc/src/html/template/form/form.cn.js
@@ -1,3 +1,18 @@
 module.exports = {
-  fieldset: []
+  fieldset: [{
+    name: "rootId",
+    label: "路径id",
+    check: "required",
+    type: "text",
+    msg: [{ name: "required", value: "必填项" }]
+  }, {
+    name: "path",
+    label: "物理路径",
+    check: "required",
+    type: "text",
+    msg: [{ name: "required", value: "必填项" }]
+  }, {
+    type: "btn",
+    label: "提交"
+  }]
 }
